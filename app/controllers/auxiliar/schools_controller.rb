1de0482ca5426cbe884e5a63f77082d97bf1b9e8
Azocar <daniel.azocar18@gmail.com>
2015-07-27 17:33:07 -0430

    Separates the select params to retrieve only necessary information

--- app/controllers/auxiliar/schools_controller.rb
+++ app/controllers/auxiliar/schools_controller.rb
@@ -9,19 +9,19 @@ class Auxiliar::SchoolsController < ApplicationController
       range = params[:range].to_i
       category = params[:category].to_i
       event_types = params[:event_types].to_i
+      graphic = params[:for_graphic].present?
       @events = []
 
       set_relationship_params(range, category, event_types)
 
-      # events = EventSchoolRelationship.includes(:event).joins(:event)
-      #                 .where(school_id: school_id, created_at: @date_range,
-      #                   events: {event: @selected_events, category: @category_name})
+      select_params = "events.event, events.created_at"
+      select_params << ", events.email, events.additional_arguments" if !graphic
 
-      @events = Event.select("events.event, events.created_at")
+      @events = Event.select(select_params)
                 .joins(:event_school_relationship)
                   .where(event: @selected_events, category: @category_name,
                           created_at: @date_range,
-                            event_school_relationships: {school_id: school_id})
+                          event_school_relationships: {school_id: school_id})
 
       render json: Oj.dump(@events, mode: :compat)
       # render template: 'auxiliar/schools/show.json.jbuilder'
