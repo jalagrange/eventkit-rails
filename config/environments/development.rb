aca4cbb9b9c1826b21b40afa465c850478b6d8bb
Richard Martini <martini.richard@gmail.com>
2020-04-23 18:45:28 -0300

    Update other gems & setup assets & timezone preserves

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ EventKit::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.digest = true
 end
